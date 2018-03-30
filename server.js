80c8f1719346f0fcb5d247888866a30db3e3a7c1
zhouwei <zhou21192345@163.com>
2018-03-30 15:23:17 +0800

    upload

--- server.js
+++ server.js
@@ -2,23 +2,13 @@
 
 'use strict';
 let AV = require('leanengine');
-//外国
-/*AV.init({
-    appId: process.env.LEANCLOUD_APP_ID || 'SceUIJ0ESp2dB0JGiYnrLKEj-MdYXbMMI',
-    appKey: process.env.LEANCLOUD_APP_KEY || '48D0p3Kc3qJtuU9zHcDh1GGQ',
-    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'YPnxDbjyJzbUl5RFOaQHGLIm'
-});*/
 
-//中国
 AV.init({
     appId: process.env.LEANCLOUD_APP_ID || '43ErKK5cM1M6K1ShXFJQRqE2-gzGzoHsz',
     appKey: process.env.LEANCLOUD_APP_KEY || 'oN7218Lkutd0zqBMI06vKC6U',
     masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || '9NsvzREuHjJoOnxaA95kSuXA'
 });
 
-
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
 let app = require('./app');
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
